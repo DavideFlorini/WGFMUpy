2bec35397234c03103512f8e7eecdfc7499e0365
DFlorini <davideflorini92@gmail.com>
2025-05-19 17:32:35 -0400

    try to load the wgfmu.dll from the system, if not try to load the dll from the python package (along with its dependencies)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='WGFMUpy',
-    version='0.0.3',
+    version='0.0.3a',
     packages=find_packages(),
     include_package_data=True,
     package_data={
